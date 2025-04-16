87b3782e0b98fcfef787976f0dadf9ae36092a77
weekendact <rjs740229@naver.com>
2025-04-16 16:10:02 +0900

    fix: difficulty 이용해서 티어 매기기, (임시) userId로 username 가져오기

--- src/main/java/com/coding/backend/user/controller/UserController.java
+++ src/main/java/com/coding/backend/user/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.coding.backend.user.controller;
 
+import com.coding.backend.user.service.UserService;
+import com.coding.backend.z.tools.ResultDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{userId}/name")
+    public ResponseEntity<ResultDto<String>> getUsername(@PathVariable Integer userId) {
+        String username = userService.getUsernameById(userId);
+        return ResponseEntity.ok(ResultDto.of(HttpStatus.OK, "유저 이름 조회 성공", username, "username"));
+    }
 }
--- src/main/java/com/coding/backend/user/service/UserService.java
+++ src/main/java/com/coding/backend/user/service/UserService.java
@@ -1,4 +1,20 @@
 package com.coding.backend.user.service;
 
+import com.coding.backend.global.exception.ResourceNotFoundException;
+import com.coding.backend.user.entity.User;
+import com.coding.backend.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public String getUsernameById(Integer userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("해당 유저를 찾을 수 없습니다."));
+        return user.getName();
+    }
 }
