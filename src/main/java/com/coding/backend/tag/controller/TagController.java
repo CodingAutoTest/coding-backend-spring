878f69c00eb44a68b430a63a261fa84fff2a4204
HanSeun9Hyun <hgjasdf7@naver.com>
2025-05-07 14:26:06 +0900

    feat: tag 조회 기능 구현

--- src/main/java/com/coding/backend/tag/controller/TagController.java
+++ src/main/java/com/coding/backend/tag/controller/TagController.java
@@ -1,4 +1,30 @@
 package com.coding.backend.tag.controller;
 
+import com.coding.backend.tag.dto.TagDto;
+import com.coding.backend.tag.service.TagService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tags")
 public class TagController {
+
+    private final TagService tagService;
+
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TagDto>> getTags() {
+            List<TagDto> tags = tagService.getAllTags();
+            return ResponseEntity.ok(tags);
+    }
 }
+
+
--- src/main/java/com/coding/backend/tag/service/TagService.java
+++ src/main/java/com/coding/backend/tag/service/TagService.java
@@ -1,4 +1,27 @@
 package com.coding.backend.tag.service;
 
+import com.coding.backend.tag.dto.TagDto;
+import com.coding.backend.tag.entity.Tag;
+import com.coding.backend.tag.repository.TagRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class TagService {
+
+    private final TagRepository tagRepository;
+
+    public TagService(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+    public List<TagDto> getAllTags() {
+        List<Tag> tags = tagRepository.findAll();
+        return tags.stream()
+                .map(tag -> new TagDto(tag.getId(), tag.getName()))
+                .collect(Collectors.toList());
+    }
 }
+
